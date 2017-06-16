fd3c37a6eec6b784294976a90f445c9d4d56f5c6
0ry <0ry.its@gmail.com>
2017-06-16 17:36:08 -0300

    Create a button-counter and update README

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # elm-play
 Learning the Elm language
+
+## Tooling and dynamics of coding
+I'm using [Visual Studio Code][editor] editor with the [`elm` plugin][plugin] by **Sascha Brink**. In it you can type inside a `.elm` file 
+``` Elm
+Html.program
+```
+and hit tab to be welcomed by a basic template (while typing look for the hint given for auto-completing, just below the cursor).
+
+
+A little bit of tuning in
+  - [x] **Model** type
+  - [x] **Msg** type
+  - [x] **update** function
+  - [x] **view** function
+  - [x] **init** call
+and you can contemplate your own Elm web app (or page?). Of course that you can further tune the code customizing all those parts and the subscriptions function.
+
+
+[editor]: https://code.visualstudio.com/ "VS Code"
+[plugin]: https://github.com/Krzysztof-Cieslak/vscode-elm "Elm plugin"
---
+++ btn-cnt.elm
@@ -0,0 +1,58 @@
+module Name exposing (Model, Msg, update, view, subscriptions, init)
+
+
+import Html exposing (..)
+import Html.Events exposing (onClick)
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+    }
+
+
+type alias Model =
+    { points : Int
+    }
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        Increment ->
+            ( { model | points = model.points + 1 }, Cmd.none)
+
+        Decrement ->
+            ( { model | points = model.points + -1 }, Cmd.none)
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Increment ] [ text "+" ]
+        , text ( toString model.points )
+        , button [ onClick Decrement ] [ text "-" ]
+        , div [] []
+        , button [ onClick Increment ] [ text "+" ]
+        , div [] [ text ( toString model.points ) ]
+        , button [ onClick Decrement ] [ text "-" ]
+        ]
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+init : (Model, Cmd Msg)
+init = 
+    ( { points = 0 }, Cmd.none)
