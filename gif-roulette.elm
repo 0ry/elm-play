e0aab01a814f441648636cba1737147394006623
0ry <0ry.its@gmail.com>
2017-06-17 18:23:14 -0300

    Roulette the gifs

--- gif-roulette.elm
+++ gif-roulette.elm
@@ -1,7 +1,8 @@
-
-import Html exposing (Html, div, h2, text, img, button)
-import Html.Events exposing (onClick)
-import Html.Attributes exposing (src)
+import Html exposing (Html, div, h2, text, img, button, input)
+import Html.Events exposing (onClick, onInput)
+import Html.Attributes exposing (..)
+import Http
+import Json.Decode as Decode
 
 main : Program Never Model Msg
 main = 
@@ -15,19 +16,13 @@ main =
 type alias Model = 
     { topic : String
     , gifUrl : String
+    , alert : String
     }
 
 type Msg
     = SetTopic String
-    | ChangeUrl
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ h2 [] [ text model.topic ]
-        , img [ src model.gifUrl ] []
-        , button [ onClick ChangeUrl ] [ text "Change Gif" ]
-        ]
+    | MorePlease
+    | NewGif (Result Http.Error String)
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -35,9 +30,26 @@ update msg model =
         SetTopic str ->
             ({ model | topic = str }, Cmd.none)
 
-        ChangeUrl ->
-            (model, Cmd.none)
-            -- ({ model | gifUrl = str }, Cmd.none)
+        MorePlease ->
+            ({ model | alert = "Loading..." }, getRandomGif model.topic)
+
+        NewGif (Ok newUrl) ->
+            ({ model | gifUrl = newUrl, alert = "" }, Cmd.none)
+
+        NewGif (Err errMsg) ->
+            ({ model | alert = (toString errMsg) }, Cmd.none)
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h2 [] [ text model.topic ]
+        , img [ src model.gifUrl ] []
+        , div []
+            [ button [ onClick MorePlease ] [ text "More Please" ]
+            , input [ type_ "text", placeholder "Topic", onInput SetTopic ] []
+            ]
+        , div [] [ text model.alert ]
+        ]
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
@@ -45,4 +57,19 @@ subscriptions model =
 
 init : (Model, Cmd Msg)
 init =
-    (Model "" "", Cmd.none)
+    (Model "1 Die" "images/die_1.png" "", Cmd.none)
+
+getRandomGif : String -> Cmd Msg
+getRandomGif topic =
+    let
+        url =
+            "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" ++ topic
+
+        request =
+            Http.get url decodeGifUrl
+    in
+        Http.send NewGif request
+
+decodeGifUrl : Decode.Decoder String
+decodeGifUrl =
+    Decode.at ["data", "image_url"] Decode.string
